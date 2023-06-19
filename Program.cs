ad8cf2bb5f521bc8547f255ef5701d7f8b8c2165
Linas Norkus <linasnorkus92@gmail.com>
2023-06-19 21:38:29 +0300

    Made another class called "Excavator"

--- Program.cs
+++ Program.cs
@@ -10,21 +10,35 @@ namespace InheritanceProject
     {
         static void Main(string[] args)
         {
-
+            List<InventoryItemModel> inventory = new List<InventoryItemModel>();
+            inventory.Add(new VehicleModel { DealerFee = 25, ProductName = "Nissan"});
+            inventory.Add(new BookModel { ProductName = "Harry Potter", NumberOfPages = 500});
 
             Console.ReadLine();
         }
     }
+
     public class InventoryItemModel
     {
         public string ProductName { get; set; }
     }
+
     public class VehicleModel: InventoryItemModel
     {
         public decimal DealerFee { get; set; }
     }
+
     public class BookModel: InventoryItemModel
     {
         public int NumberOfPages { get; set; }
     }
+
+    public class Excavator: InventoryItemModel
+    {
+        public void Dig()
+        {
+            Console.WriteLine("I DIG!");
+        }
+    }
 }
+
