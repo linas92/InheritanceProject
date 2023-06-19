930a02e307643e6683429f64f20e007964b9563e
Linas Norkus <linasnorkus92@gmail.com>
2023-06-19 21:35:17 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.ReadLine();
+        }
+    }
+    public class InventoryItemModel
+    {
+        public string ProductName { get; set; }
+    }
+    public class VehicleModel: InventoryItemModel
+    {
+        public decimal DealerFee { get; set; }
+    }
+    public class BookModel: InventoryItemModel
+    {
+        public int NumberOfPages { get; set; }
+    }
+}
